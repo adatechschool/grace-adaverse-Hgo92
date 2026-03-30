641789e2eb5b38f7715204c3fc759edae515a52e
Hgo92 <hugo_monier@yahoo.fr>
2026-03-30 11:13:52 +0200

    ajout schema.ts dans db

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,8 +1,27 @@
-import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
+import { integer, pgTable, varchar, boolean, timestamp, text } from "drizzle-orm/pg-core";
 
-export const usersTable = pgTable("users", {
+export const studentsProjects = pgTable("students_projects", {
+    id: integer().primaryKey().generatedAlwaysAsIdentity(),
+    ada_project_id : integer('ada_project_id').references(() => adaProjects.id).notNull(),
+    promo_id : integer('promo_id').references(() => promos.id).notNull(),
+    name: varchar({ length: 50 }).notNull(),
+    description : varchar({length : 255}).notNull(),
+    img : text("img").notNull(),
+    weblink: text("weblink").notNull(),
+    github: text("github").notNull(),
+    creationDate : timestamp("creation_date").notNull(),
+    valid : boolean("valid").notNull(),
+    publicationDate : timestamp("publication_date")
+});
+
+export const promos = pgTable("promos", {
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
-  name: varchar({ length: 255 }).notNull(),
-  age: integer().notNull(),
-  email: varchar({ length: 255 }).notNull().unique(),
+  name: varchar({ length: 50 }).notNull(),
+  startdate : timestamp().notNull()
+  
+});
+
+export const adaProjects = pgTable("ada_projects", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  name: varchar({ length: 50 }).notNull()
 });
