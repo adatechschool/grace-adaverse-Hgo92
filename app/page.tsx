2b834addca038e030fd3bf4156fa7d55ce342647
Hgo92 <hugo_monier@yahoo.fr>
2026-03-31 18:51:05 +0200

    Modification home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,20 @@
-import Image from "next/image";
+import ButtonHome from "./components/ButtonHome";
+import { db } from "@/src";
+import { studentsProjects, promos, adaProjects } from "../src/db/schema";
 
-export default function Home() {
-  return (
-    <div>Coucou
+export  default async function Home() {
+
+const promosAda = await db.select().from(promos);
+const projectsAda = await db.select().from(adaProjects);
+  
+return (
+    <div>
+      <h1>Adaverse</h1>
+      <p>Découvrez les projets des différentes promotions d'Ada Tech School !</p>
+      <ButtonHome 
+        projectsAda={projectsAda}
+        promosAda={promosAda}
+        />
     </div>
   );
 }
