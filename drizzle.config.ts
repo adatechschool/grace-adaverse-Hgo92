70d2beaeb9f9fa1b57ecca5516a29d2de8e3beaa
Hgo92 <hugo_monier@yahoo.fr>
2026-03-31 18:50:41 +0200

    modif drizzle config fix bug

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,19 +1,10 @@
-import 'dotenv/config'; // This is the magic line
 import { defineConfig } from 'drizzle-kit';
-import { config } from 'dotenv';
-
-config({ path: '.env.local' });
-
-// Check if the URL is actually loading
-if (!process.env.DATABASE_URL) {
-  throw new Error('DATABASE_URL is missing in drizzle.config.ts');
-}
 
 export default defineConfig({
   out: './drizzle',
   schema: './src/db/schema.ts', 
   dialect: 'postgresql',
   dbCredentials: {
-    url: process.env.DATABASE_URL,
+    url: process.env.DATABASE_URL!, // The '!' tells TS you know it's there
   },
 });
