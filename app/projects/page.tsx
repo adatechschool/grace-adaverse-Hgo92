e9fb3efff5cf44e960c06f01c7be7397f3595a21
Hgo92 <hugo_monier@yahoo.fr>
2026-04-01 22:20:50 +0200

    affichage des projets

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,37 +1,54 @@
 import Navbar from "../_components/Navbar";
 import { ProjectsProps, PromosProps, StudentProjectsProps } from "@/src/interface/interface";
+import { db } from "@/src";
+import { studentsProjects, promos, adaProjects } from "@/src/db/schema";
+import { eq, and } from "drizzle-orm";
 
-interface ProjectPageProps {
-    projectsAda : ProjectsProps[];
-    promosAda : PromosProps[]
-    studentsProjects : StudentProjectsProps[]
-    promoId : number | string,
-    projectId : number | string
+function isNumber(prop : number | "tous") : prop is number {
+    return typeof prop === "number"
 }
 
-export default function projects({projectsAda, promosAda, projectId, promoId, studentsProjects} : ProjectPageProps) {
+type RawSearchParams = { projectId: string; promoId: string };
+
+export default async function projects({ searchParams }: { searchParams: Promise<RawSearchParams> ;}) {
+    const params  =  await searchParams;
+    
+    const projectId = params.projectId != "tous" ? parseInt(params.projectId) : "tous";
+    const promoId = params.promoId != "tous" ? parseInt(params.promoId) : "tous";
+    const filters = [];
+
+    const promosAda : PromosProps[] = await db.select().from(promos);
+    const projectsAda : ProjectsProps[] = await db.select().from(adaProjects);
+
+    if (isNumber(projectId)) { filters.push(eq(studentsProjects.ada_project_id, projectId));} 
+    if (isNumber(promoId)) { filters.push(eq(studentsProjects.promo_id, promoId))}
+    
+    const studentsProjectsAda : StudentProjectsProps[] = await db.select().from(studentsProjects).where(and(...filters));
 
     return(
         <div>
             <Navbar/>
-            {projectsAda.map((project) => {
-                if (project.id === projectId || projectId === "tous") {
+            {projectsAda
+            .filter(project => projectId === "tous" || project.id === projectId)
+            .map((project) => {
+                const filtreProjets = studentsProjectsAda.filter((sp) => sp.ada_project_id === project.id)
                 return (
                     <div>
                         <h2>{project.name}</h2>
-                        {studentsProjects.map((studentProject) => {
-                            if (studentProject.promo_id === promoId && studentProject.ada_project_id === project.id) {
-                                return (
-                                    <div>
-                                        <img src={studentProject.img}></img>
-                                        <span>{promosAda[promoId-1].name}</span>
+                        <div>
+                        {filtreProjets.map((studentProject) => {
+                                 return (
+                                    <div key={studentProject.id}>
+                                        <img src={studentProject.img} alt={studentProject.name}></img>
+                                        <span>{promosAda.find(p => p.id === studentProject.promo_id)?.name}</span>
                                         <h3>{studentProject.name}</h3>
                                         <span>{studentProject.publicationDate.toDateString()}</span>
                                     </div>
-                                )}
+                                )
                         })}
+                        </div>
                     </div>
-                )}
+                )
             })}
         </div>
     )
