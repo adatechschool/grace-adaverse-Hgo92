adbc732326016f18543d525de249305bebcc46d1
Hgo92 <hugo_monier@yahoo.fr>
2026-04-01 13:43:09 +0200

    ajout div projets

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -5,8 +5,8 @@ interface ProjectPageProps {
     projectsAda : ProjectsProps[];
     promosAda : PromosProps[]
     studentsProjects : StudentProjectsProps[]
-    promoId : number,
-    projectId : number
+    promoId : number | string,
+    projectId : number | string
 }
 
 export default function projects({projectsAda, promosAda, projectId, promoId, studentsProjects} : ProjectPageProps) {
@@ -14,7 +14,25 @@ export default function projects({projectsAda, promosAda, projectId, promoId, st
     return(
         <div>
             <Navbar/>
-
+            {projectsAda.map((project) => {
+                if (project.id === projectId || projectId === "tous") {
+                return (
+                    <div>
+                        <h2>{project.name}</h2>
+                        {studentsProjects.map((studentProject) => {
+                            if (studentProject.promo_id === promoId && studentProject.ada_project_id === project.id) {
+                                return (
+                                    <div>
+                                        <img src={studentProject.img}></img>
+                                        <span>{promosAda[promoId-1].name}</span>
+                                        <h3>{studentProject.name}</h3>
+                                        <span>{studentProject.publicationDate.toDateString()}</span>
+                                    </div>
+                                )}
+                        })}
+                    </div>
+                )}
+            })}
         </div>
     )
 }
