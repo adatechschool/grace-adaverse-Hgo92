7edd281b284920aaf161b6d3ad55f9ef3f9abd44
Hgo92 <hugo_monier@yahoo.fr>
2026-04-01 10:41:25 +0200

    Ajout d'une page pour mes interfaces

---
+++ src/interface/interface.tsx
@@ -0,0 +1,31 @@
+// Interface pour ma data de promo
+
+export interface PromosProps {
+    id: number;
+    name: string;
+    startdate: string;
+};
+
+// Interface pour ma data des types de projets
+
+export interface ProjectsProps {
+    id: number;
+    name: string;
+};
+
+// Interface pour ma data des projets
+
+export interface StudentProjectsProps {
+    id: number;
+    ada_project_id: number;
+    promo_id: number;
+    name: string;
+    description: string;
+    img: string;
+    weblink: string;
+    github: string;
+    demo: string | null;
+    creationDate: Date;
+    valid: boolean;
+    publicationDate: Date | null;
+}
