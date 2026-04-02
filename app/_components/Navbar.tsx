d787119d8f4bf7953e70d81a227636db7de9ea88
Hgo92 <hugo_monier@yahoo.fr>
2026-04-02 15:58:05 +0200

    Update Navbar

--- app/_components/Navbar.tsx
+++ app/_components/Navbar.tsx
@@ -1,6 +1,37 @@
-export default function Navbar() {
+"use client";
+import Link from "next/link";
+import SelectProjects from "./_modules/SelectProjects";
+import SelectPromos from "./_modules/SelectPromos";
+import { useState } from "react";
+import { SelectType } from "./ButtonHome";
+import { ProjectsProps,PromosProps } from "@/src/interface/interface";
+
+interface NavProps {
+    projectsAda : ProjectsProps[];
+    promosAda : PromosProps[]
+}
+
+
+export default function Navbar({projectsAda, promosAda} : NavProps ) {
+
+    const [projectId, setProjectId] = useState<SelectType>("tous");
+    const [promoId, setPromoId] = useState<SelectType>("tous");
+
     return (
-        <div>Ceci sera ma Navbar</div>
+        <div>
+            <Link href="./">Adaverse</Link>
+            <SelectProjects
+                projectId = {projectId}
+                setProjectId = {setProjectId}
+                projectsAda = {projectsAda}
+            />
+            <SelectPromos 
+                promoId = {promoId}
+                setPromoId = {setPromoId}
+                promosAda = {promosAda}
+            />
+            <Link href={`./projects?projectId=${projectId}&promoId=${promoId}`}>Voir</Link>
+        </div>
         
     )
 }
