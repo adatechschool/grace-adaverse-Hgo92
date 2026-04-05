fcbdfa7bc554b1b96b40a4d620b40aa18df41f88
Hgo92 <hugo_monier@yahoo.fr>
2026-04-05 19:05:42 +0200

    changement du seed pour intégrer les fichiers sql

--- src/db/seed.ts
+++ src/db/seed.ts
@@ -1,58 +1,22 @@
 import { db } from "../index";
-import 'dotenv/config';
-import { promos, adaProjects } from "./schema";
+import fs from 'fs';
+import path from "path";
+import { sql } from "drizzle-orm";
 
+async function checkSql(fileName : string) {
+    const filePath = path.join(__dirname, 'queries', fileName);
+    const query = fs.readFileSync(filePath, 'utf8');
 
+    await db.execute(sql.raw(query));
+}
 
 async function main() {
-  const promo: typeof promos.$inferInsert[] = [{
-    name: 'Grace Hopper',
-    startdate : '2025-10-01'
-  },{
-    name : 'Frida Khalo',
-    startdate : '2025-05-01'
-  }, 
-  {
-    name : 'Gisèle Halimi',
-    startdate : '2026-01-15'
-  }
-];
-
-  await db.insert(promos).values(promo);
-  console.log('Les promos ont été créées'); 
-
-    const projectada: typeof adaProjects.$inferInsert[] = [
-        {
-            name : 'Adaquiz'
-        }, 
-        {
-            name : 'Adabilan'
-        },
-        {
-            name : 'Adapage'
-        }, 
-        {
-            name : 'Adashboard'
-        }, 
-        {
-            name : 'Adataviz'
-        },
-        {
-            name : 'Adatabase'
-        }, 
-        {
-            name : 'Adapi'
-        }, 
-        {
-            name : 'Nextada'
-        }, 
-        {   name : 'Adaverse'
-        }
-    ];
-
-    await db.insert(adaProjects).values(projectada);
-    console.log('Les projets Ada ont été créés'); 
-
+    try {
+        await checkSql('projectsAda.sql');
+        await checkSql('promos.sql');
+    } catch (error) {
+        console.error("Le seed n'a pas marché :", error);
+    }
 }
 
 main()
