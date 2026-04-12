6930764a1f5c80cdc4a247cc733ad51ce6669364
Hgo92 <hugo_monier@yahoo.fr>
2026-04-12 17:12:33 +0200

    ajout d'une colonne view dans la table des projets

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -12,7 +12,8 @@ export const studentsProjects = pgTable("students_projects", {
     github: text("github").notNull(),
     demo : text("demo"),
     creationDate : timestamp("creation_date").notNull().defaultNow(),
-    publicationDate : timestamp("publication_date")
+    publicationDate : timestamp("publication_date"),
+    views : integer('views').notNull().default(0)
 });
 
 export const promos = pgTable("promos", {
