5385effbde890c688f67de7114e3d999f14caed2
Hgo92 <hugo_monier@yahoo.fr>
2026-04-13 23:47:12 +0200

    ajouts projets dans le seed

--- src/db/seed-projets.ts
+++ src/db/seed-projets.ts
@@ -6,6 +6,50 @@ import { sql } from "drizzle-orm";
 async function main() {
     const projet : typeof studentsProjects.$inferInsert[] = [
         {
+            ada_project_id : 4,
+            promo_id : 1,
+            name: "Adashboard - Hugo M.",
+            description : "Un dashboard pour afficher mes compétences et les faire évoluer au fil de ma formation.",
+            img : "https://github.com/adatechschool/grace-adashboard-Hgo92/blob/main/thumbnail.png",
+            weblink: "grace-adashboard-hugo",
+            github: "https://github.com/adatechschool/grace-adashboard-Hgo92",
+            demo : "https://grace-adashboard-hgo92.vercel.app/",
+            publicationDate : sql`now()` as unknown as Date
+        },
+                {
+            ada_project_id : 3,
+            promo_id : 1,
+            name: "Grace Hopper",
+            description : "Un page réalisée en React pour découvrir la vie passionnante de Grace Hopper, pionnière de l'informatique qui a donné son nom à notre promotion.",
+            img : "https://github.com/Hgo92/adapage-zineb-hugo/blob/main/thumbnail.png",
+            weblink: "grace-adapage-hopper",
+            github: "https://github.com/Hgo92/adapage-zineb-hugo/tree/main",
+            demo : "https://adapage-zineb-hugo.vercel.app/",
+            publicationDate : sql`now()` as unknown as Date
+        },
+                {
+            ada_project_id : 5,
+            promo_id : 1,
+            name: "La Mémoire de Paris",
+            description : "Pour ce projet de Dataviz, j'ai choisi les plaques commémoratives. A Paris, ce sont 2 575 plaques qui ornent murs et monuments. Découvrez-les ! Un projet réalisé par Hugo Monier.",
+            img : "https://github.com/Hgo92/grace-adataviz-hgo92/blob/main/thumbnail.png",
+            weblink: "grace-adataviz-memoire",
+            github: "https://github.com/Hgo92/grace-adataviz-hgo92",
+            demo : "https://grace-adataviz-hgo92.vercel.app/",
+            publicationDate : sql`now()` as unknown as Date
+        },
+                {
+            ada_project_id : 4,
+            promo_id : 1,
+            name: "Learn With Me",
+            description : "Un application de flashcards pour aider à l'apprentissage des langues ! Un projet réalisé par Hugo Monier.",
+            img : "https://github.com/Hgo92/learn-with-me-hgo92/blob/main/front/learn-with-me/thumbnail.png",
+            weblink: "grace-learnwithme-hugo",
+            github: "https://github.com/Hgo92/learn-with-me-hgo92/tree/main",
+            demo : "https://learn-with-me-hgo92.vercel.app/",
+            publicationDate : sql`now()` as unknown as Date
+        },
+        {
             ada_project_id : 5,
             promo_id : 1,
             name: "Arbres remarquables de Paris",
