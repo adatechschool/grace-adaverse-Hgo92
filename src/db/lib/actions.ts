fbea488962512fa0cc6bf6ff73f670b50fc22572
Hgo92 <hugo_monier@yahoo.fr>
2026-04-14 10:42:56 +0200

    annulation du changement des select car ça plantait

--- src/db/lib/actions.ts
+++ src/db/lib/actions.ts
@@ -7,19 +7,24 @@ import { ProjectSchema } from "./zod-schemas";
 
 export async function addProject(formData : FormData) {
 
+    // Je recupère des éléments de mon forme pour créer mes propriétés weblink et img correspondantes
+    const projectId = await db.select({ name: adaProjects.name }).from(adaProjects).where(eq(adaProjects.id, Number(formData.get('ada_project_id'))));
+    const promoId = await db.select({name : promos.name}).from(promos).where(eq(promos.id, Number(formData.get('promo_id'))));
+    const promoIdCut = promoId[0].name.split(" ")
+    const weblink = (promoIdCut[0] + "-" + projectId[0].name + "-" + formData.get('name')).toLowerCase();
+    console.log(weblink)
+    const img = formData.get('github') + "/blob/main/thumbnail.png?raw=true";
+    console.log(img)
+
     // Je transforme la date de mon form en objet pour la valider avec Zod
 
     const dataToValidate = Object.fromEntries(formData.entries());
+    console.log(dataToValidate);
     const validatedData = ProjectSchema.safeParse(dataToValidate);
+    console.log(validatedData.data);
+    
 
     if (validatedData.success) {
-
-            // Je recupère des éléments de mon forme pour créer mes propriétés weblink et img correspondantes
-        const projectId = await db.select({ name: adaProjects.name }).from(adaProjects).where(eq(adaProjects.id, Number(formData.get('ada_project_id'))));
-        const promoId = await db.select({name : promos.name}).from(promos).where(eq(promos.id, Number(formData.get('promo_id'))));
-        const promoIdCut = promoId[0].name.split(" ")
-        const weblink = (promoIdCut[0] + "-" + projectId[0].name + "-" + formData.get('name')).toLowerCase();
-        const img = formData.get('github') + "/blob/main/thumbnail.png?raw=true";
         console.log("ça devrait être bon")
         await db.insert(studentsProjects).values({
         ...validatedData.data,
